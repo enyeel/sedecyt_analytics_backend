c6aed20ea3f3df268e9eb1edc33dc71e80d2ef4f
Angel Gutierrez <st4341@utr.edu.mx>
2025-10-04 11:34:58 -0600

    Modificacion html e implementacion de las rutas de flask

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,  request
+from ..services.google_sheets_service import save_user_data
+
 
 main = Blueprint("main", __name__)
 
@@ -6,6 +8,32 @@ main = Blueprint("main", __name__)
 def home():
     return render_template("login.html")
 
-@main.route("/ping")
-def ping():
-    return jsonify({"status": "ok"})
+@main.route("/registro", methods=["GET", "POST"])
+def registro():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        if password != confirm_password:
+            error_message = "Las contraseñas no coinciden."
+            return render_template("registro.html", error=error_message)
+        
+    user_data = [username, email, password]
+
+    exito = save_user_data(
+        credentials_path="C:/Users/angel/OneDrive/Escritorio/estadias/SEDECYT_ANALYTICS/credentials.json",
+        sheet_id="1gk3y3YH2bX4t5v6w7x8y9z0A1B2C3D4E5F6G7H8I9J0K",
+        worksheet_name="users",
+        user_data=user_data
+    )
+
+    if exito:
+        return render_template("login.html", message="Registro exitoso. Ahora puedes iniciar sesión.")
+    
+    else:
+        error_message = "Error al registrar el usuario. Inténtalo de nuevo."
+        return render_template("registro.html", error=error_message)
+
+    return render_template("registro.html")
