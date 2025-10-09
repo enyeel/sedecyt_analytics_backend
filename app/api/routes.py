e78cfae00e0e338631ef348fe9d78f8d1c98aa42
Angel Gutierrez <st4341@utr.edu.mx>
2025-10-09 09:21:36 -0600

    correcion de ruta registro

--- app/api/routes.py
+++ app/api/routes.py
@@ -20,20 +20,20 @@ def registro():
             error_message = "Las contraseñas no coinciden."
             return render_template("registro.html", error=error_message)
         
-    user_data = [username, email, password]
-
-    exito = save_user_data(
-        credentials_path="C:/Users/angel/OneDrive/Escritorio/estadias/SEDECYT_ANALYTICS/credentials.json",
-        sheet_id="1gk3y3YH2bX4t5v6w7x8y9z0A1B2C3D4E5F6G7H8I9J0K",
-        worksheet_name="users",
-        user_data=user_data
-    )
-
-    if exito:
-        return render_template("login.html", message="Registro exitoso. Ahora puedes iniciar sesión.")
+        user_data = [username, email, password]
     
-    else:
-        error_message = "Error al registrar el usuario. Inténtalo de nuevo."
-        return render_template("registro.html", error=error_message)
+        exito = save_user_data(
+            credentials_path="C:/Users/angel/OneDrive/Escritorio/estadias/SEDECYT_ANALYTICS/credentials.json",
+            sheet_id="1gk3y3YH2bX4t5v6w7x8y9z0A1B2C3D4E5F6G7H8I9J0K",
+            worksheet_name="users",
+            user_data=user_data
+        )
+    
+        if exito:
+            return render_template("login.html", message="Registro exitoso. Ahora puedes iniciar sesión.")
+        
+        else:
+            error_message = "Error al registrar el usuario. Inténtalo de nuevo."
+            return render_template("registro.html", error=error_message)
 
     return render_template("registro.html")
