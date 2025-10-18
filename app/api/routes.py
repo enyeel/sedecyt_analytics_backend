935253d14111c6e65f8dbdc2403f9ff8b4a21e0d
Angel Gutierrez <st4341@utr.edu.mx>
2025-10-17 20:32:10 -0600

    implemantacion del endpoint para obtener todos los registros de una tabla

--- app/api/routes.py
+++ app/api/routes.py
@@ -17,7 +17,6 @@ def grafico1():
 
 @api_bp.route("/chart/grafico1_auth", methods=["GET"])
 @token_required
-
 def grafico1_auth():
     # Lógica para obtener y procesar los datos del gráfico 1
     data = {
@@ -28,7 +27,19 @@ def grafico1_auth():
  
  
 @api_bp.route("/health", methods = ["GET"])
- 
 def health_check():
     return jsonify({"status": "API desplegada automaticamente desde Git"}), 200
 
+
+@api_bp.route("tabla/<string:table_name>", methods=["GET"])
+def get_table_data(table_name):
+    from app.services.supabase_service import get_all_from
+    
+    print(f"Petición para obtener datos de la tabla: {table_name}")
+
+    data = get_all_from(table_name)
+    
+    if isinstance(data, dict) and "error" in data:
+        return jsonify(data), 404
+    
+    return jsonify(data), 200
