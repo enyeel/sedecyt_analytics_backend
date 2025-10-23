8f6a4a5e7f211ff85bdbbbc91c2a88ba09962d01
enyeel <anjel.hdz22@gmail.com>
2025-10-23 13:50:50 -0600

    arreglado conflicto

--- app/api/routes.py
+++ app/api/routes.py
@@ -27,7 +27,6 @@ def get_table_data(table_name):                                 #el nombre de la
     
     return jsonify(data), 200
 
-<<<<<<< HEAD
 # Endpoint para testing de ETL script
 @api_bp.route("/run-etl", methods=["GET"])
 def run_etl():
@@ -40,35 +39,3 @@ def run_etl():
         return jsonify(result), 500
 
     return jsonify(result), 200 
-=======
-#endpoint de prueba para verificar conexión con supabase
-@api_bp.route("tabla-no-auth/<string:tabla>", methods=["GET"])
-def get_table(tabla):                                 #el nombre de la tabla se pasa como parámetro en la URL después de /tabla/
-    from app.services.supabase_service import get_all_from
-    
-    print(f"Petición para obtener datos de la tabla: {tabla}")
-
-    data = get_all_from(tabla)
-    
-    if isinstance(data, dict) and "error" in data:
-        return jsonify(data), 404
-    
-    return jsonify(data), 200
-
-# Endpoint de prueba para verificar la conexión con Google Sheets
-
-@api_bp.route("/sheets", methods=['GET'])
-@token_required
-def get_contactos_from_sheet():
-    from ..services import google_sheets_service
-
-    print("Petición para obtener datos de Google Sheets")
-
-    try:
-        # Solo le pasas el nombre de la pestaña, ¡y listo!
-        df_contactos = google_sheets_service.read_worksheet_as_dataframe("Formulario Desarrollo Industria")
-        contactos_json = df_contactos.to_dict(orient='records')
-        return jsonify(contactos_json)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
->>>>>>> 2efb001593f81d518f45e7aa01ed548b1d946f53
