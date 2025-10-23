2efb001593f81d518f45e7aa01ed548b1d946f53
Angel Gutierrez <st4341@utr.edu.mx>
2025-10-23 12:45:09 -0600

    Configuracion de la ruta para que cloud run acceda a las credenciales desde app/secrets/cretdentials.json

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 # Para Cloud Run, el archivo estará en una ruta fija.
 # Para local, podemos decirle que lo busque en la raíz del proyecto.
 # La variable de entorno tiene prioridad.
-CREDENTIALS_PATH = os.environ.get("GOOGLE_CREDENTIALS_PATH", "/app/credentials.json")
+CREDENTIALS_PATH = os.environ.get("GOOGLE_CREDENTIALS_PATH", "/app/secrets/credentials.json")
 SHEET_ID = os.environ.get("SPREADSHEET_ID")
